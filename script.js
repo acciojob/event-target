eb9658773b29afeb51bf7a76843a00d99be6bed3
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-06 09:28:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+class EventTarget {
+    // Write your code here.
+  
+    addEventListener(name, callback) {
+      // Write your code here.
+    }
+  
+    removeEventListener(name, callback) {
+      // Write your code here.
+    }
+  
+    dispatchEvent(name) {
+      // Write your code here.
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.EventTarget = EventTarget;
+  
